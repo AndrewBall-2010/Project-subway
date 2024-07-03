0b0619e9717475363e5e34a7290ba4e4887653be
AndrewBall-2010 <andrewm242010@gmail.com>
2024-07-03 17:23:30 -0400

    Add files via upload

--- cog.py
+++ cog.py
@@ -1,141 +1,224 @@
-import logging
-import random
-import sys
+import zipfile
+from io import BytesIO
 from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from tortoise.expressions import Q
 
-from ballsdex import __version__ as ballsdex_version
-from ballsdex.core.models import Ball
-from ballsdex.core.models import balls as countryballs
-from ballsdex.core.utils.tortoise import row_count_estimate
+from ballsdex.core.models import BallInstance, DonationPolicy
+from ballsdex.core.models import Player as PlayerModel
+from ballsdex.core.models import PrivacyPolicy, Trade, TradeObject
+from ballsdex.core.utils.buttons import ConfirmChoiceView
 from ballsdex.settings import settings
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
-log = logging.getLogger("ballsdex.packages.info")
 
-
-def mention_app_command(app_command: app_commands.Command | app_commands.Group) -> str:
-    if "mention" in app_command.extras:
-        return app_command.extras["mention"]
-    else:
-        if isinstance(app_command, app_commands.ContextMenu):
-            return f"`{app_command.name}`"
-        else:
-            return f"`/{app_command.name}`"
-
-
-class Info(commands.Cog):
+class Player(commands.GroupCog):
     """
-    Simple info commands.
+    Manage your account settings.
     """
 
     def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
-
-    async def _get_10_balls_emojis(self) -> list[discord.Emoji]:
-        balls: list[Ball] = random.choices(
-            [x for x in countryballs.values() if x.enabled], k=min(10, len(countryballs))
-        )
-        emotes: list[discord.Emoji] = []
-
-        for ball in balls:
-            if emoji := self.bot.get_emoji(ball.emoji_id):
-                emotes.append(emoji)
-
-        return emotes
+        if not self.bot.intents.members:
+            self.__cog_app_commands_group__.get_command("privacy").parameters[  # type: ignore
+                0
+            ]._Parameter__parent.choices.pop()  # type: ignore
 
     @app_commands.command()
-    async def about(self, interaction: discord.Interaction):
+    @app_commands.choices(
+        policy=[
+            app_commands.Choice(name="Open Inventory", value=PrivacyPolicy.ALLOW),
+            app_commands.Choice(name="Private Inventory", value=PrivacyPolicy.DENY),
+            app_commands.Choice(name="Same Server", value=PrivacyPolicy.SAME_SERVER),
+        ]
+    )
+    async def privacy(self, interaction: discord.Interaction, policy: PrivacyPolicy):
         """
-        Get information about this bot.
+        Set your privacy policy.
         """
-        embed = discord.Embed(
-            title=f"{settings.bot_name} Discord bot", color=discord.Colour.blurple()
+        if policy == PrivacyPolicy.SAME_SERVER and not self.bot.intents.members:
+            await interaction.response.send_message(
+                "I need the `members` intent to use this policy.", ephemeral=True
+            )
+            return
+        player, _ = await PlayerModel.get_or_create(discord_id=interaction.user.id)
+        player.privacy_policy = policy
+        await player.save()
+        await interaction.response.send_message(
+            f"Your privacy policy has been set to **{policy.name}**.", ephemeral=True
         )
 
-        try:
-            balls = await self._get_10_balls_emojis()
-        except Exception:
-            log.error("Failed to fetch 10 balls emotes", exc_info=True)
-            balls = []
+    @app_commands.command()
+    @app_commands.choices(
+        policy=[
+            app_commands.Choice(name="Accept all donations", value=DonationPolicy.ALWAYS_ACCEPT),
+            app_commands.Choice(
+                name="Request your approval first", value=DonationPolicy.REQUEST_APPROVAL
+            ),
+            app_commands.Choice(name="Deny all donations", value=DonationPolicy.ALWAYS_DENY),
+        ]
+    )
+    async def donation_policy(
+        self, interaction: discord.Interaction, policy: app_commands.Choice[int]
+    ):
+        """
+        Change how you want to receive donations from /balls give
 
-        balls_count = len([x for x in countryballs.values() if x.enabled])
-        players_count = await row_count_estimate("player")
-        balls_instances_count = await row_count_estimate("ballinstance")
-
-        assert self.bot.user
-        assert self.bot.application
-        try:
-            assert self.bot.application.install_params
-        except AssertionError:
-            invite_link = discord.utils.oauth_url(
-                self.bot.application.id,
-                permissions=discord.Permissions(
-                    manage_webhooks=True,
-                    read_messages=True,
-                    send_messages=True,
-                    manage_messages=True,
-                    embed_links=True,
-                    attach_files=True,
-                    use_external_emojis=True,
-                    add_reactions=True,
-                ),
-                scopes=("bot", "applications.commands"),
+        Parameters
+        ----------
+        policy: DonationPolicy
+            The new policy for accepting donations
+        """
+        player, _ = await PlayerModel.get_or_create(discord_id=interaction.user.id)
+        player.donation_policy = DonationPolicy(policy.value)
+        if policy.value == DonationPolicy.ALWAYS_ACCEPT:
+            await interaction.response.send_message(
+                f"Setting updated, you will now receive all donated {settings.collectible_name}s "
+                "immediately."
+            )
+        elif policy.value == DonationPolicy.REQUEST_APPROVAL:
+            await interaction.response.send_message(
+                "Setting updated, you will now have to approve donation requests manually."
+            )
+        elif policy.value == DonationPolicy.ALWAYS_DENY:
+            await interaction.response.send_message(
+                "Setting updated, it is now impossible to use "
+                f"`/{settings.players_group_cog_name} give` with "
+                "you. It is still possible to perform donations using the trade system."
             )
         else:
-            invite_link = discord.utils.oauth_url(
-                self.bot.application.id,
-                permissions=self.bot.application.install_params.permissions,
-                scopes=self.bot.application.install_params.scopes,
-            )
-        embed.description = (
-            f"{' '.join(str(x) for x in balls)}\n"
-            f"{settings.about_description}\n"
-            f"*Running version **[{ballsdex_version}]({settings.github_link}/releases)***\n\n"
-            f"**{balls_count:,}** {settings.collectible_name}s to collect\n"
-            f"**{players_count:,}** players that caught "
-            f"**{balls_instances_count:,}** {settings.collectible_name}s\n"
-            f"**{len(self.bot.guilds):,}** servers playing\n\n"
-            "This bot was made by **El Laggron**, consider supporting me on my "
-            "[Patreon](https://patreon.com/retke) :heart:\n\n"
-            f"[Discord server]({settings.discord_invite}) • [Invite me]({invite_link}) • "
-            f"[Source code and issues]({settings.github_link})\n"
-            f"[Terms of Service]({settings.terms_of_service}) • "
-            f"[Privacy policy]({settings.privacy_policy})"
-        )
-
-        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
-        v = sys.version_info
-        embed.set_footer(
-            text=f"Python {v.major}.{v.minor}.{v.micro} • discord.py {discord.__version__}"
-        )
-
-        await interaction.response.send_message(embed=embed)
+            await interaction.response.send_message("Invalid input!")
+            return
+        await player.save()  # do not save if the input is invalid
 
     @app_commands.command()
-    async def help(self, interaction: discord.Interaction):
+    async def delete(self, interaction: discord.Interaction):
         """
-        Show the list of commands from the bot.
+        Delete your player data.
         """
-        assert self.bot.user
-        embed = discord.Embed(
-            title=f"{settings.bot_name} Discord bot - help menu", color=discord.Colour.blurple()
+        view = ConfirmChoiceView(interaction)
+        await interaction.response.send_message(
+            "Are you sure you want to delete your player data?", view=view, ephemeral=True
         )
-        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
+        await view.wait()
+        if view.value is None or not view.value:
+            return
+        player, _ = await PlayerModel.get_or_create(discord_id=interaction.user.id)
+        await player.delete()
 
-        for cog in self.bot.cogs.values():
-            if cog.qualified_name == "Admin":
-                continue
-            content = ""
-            for app_command in cog.walk_app_commands():
-                content += f"{mention_app_command(app_command)}: {app_command.description}\n"
-            if not content:
-                continue
-            embed.add_field(name=cog.qualified_name, value=content, inline=False)
+    @app_commands.command()
+    @app_commands.choices(
+        type=[
+            app_commands.Choice(name=settings.collectible_name.title(), value="balls"),
+            app_commands.Choice(name="Trades", value="trades"),
+            app_commands.Choice(name="All", value="all"),
+        ]
+    )
+    async def export(self, interaction: discord.Interaction, type: str):
+        """
+        Export your player data.
+        """
+        player = await PlayerModel.get_or_none(discord_id=interaction.user.id)
+        if player is None:
+            await interaction.response.send_message(
+                "You don't have any player data to export.", ephemeral=True
+            )
+            return
+        await interaction.response.defer()
+        files = []
+        if type == "balls":
+            data = await get_items_csv(player)
+            filename = f"{interaction.user.id}_{settings.collectible_name}.csv"
+            data.filename = filename  # type: ignore
+            files.append(data)
+        elif type == "trades":
+            data = await get_trades_csv(player)
+            filename = f"{interaction.user.id}_trades.csv"
+            data.filename = filename  # type: ignore
+            files.append(data)
+        elif type == "all":
+            balls = await get_items_csv(player)
+            trades = await get_trades_csv(player)
+            balls_filename = f"{interaction.user.id}_{settings.collectible_name}.csv"
+            trades_filename = f"{interaction.user.id}_trades.csv"
+            balls.filename = balls_filename  # type: ignore
+            trades.filename = trades_filename  # type: ignore
+            files.append(balls)
+            files.append(trades)
+        else:
+            await interaction.followup.send("Invalid input!", ephemeral=True)
+            return
+        zip_file = BytesIO()
+        with zipfile.ZipFile(zip_file, "w") as z:
+            for file in files:
+                z.writestr(file.filename, file.getvalue())
+        zip_file.seek(0)
+        if zip_file.tell() > 25_000_000:
+            await interaction.followup.send(
+                "Your data is too large to export."
+                "Please contact the bot support for more information.",
+                ephemeral=True,
+            )
+            return
+        files = [discord.File(zip_file, "player_data.zip")]
+        try:
+            await interaction.user.send("Here is your player data:", files=files)
+            await interaction.followup.send(
+                "Your player data has been sent via DMs.", ephemeral=True
+            )
+        except discord.Forbidden:
+            await interaction.followup.send(
+                "I couldn't send the player data to you in DM. "
+                "Either you blocked me or you disabled DMs in this server.",
+                ephemeral=True,
+            )
 
-        await interaction.response.send_message(embed=embed)
+
+async def get_items_csv(player: PlayerModel) -> BytesIO:
+    """
+    Get a CSV file with all items of the player.
+    """
+    balls = await BallInstance.filter(player=player).prefetch_related(
+        "ball", "trade_player", "special"
+    )
+    txt = (
+        f"id,hex id,{settings.collectible_name},catch date,trade_player"
+        ",special,shiny,attack,attack bonus,hp,hp_bonus\n"
+    )
+    for ball in balls:
+        txt += (
+            f"{ball.id},{ball.id:0X},{ball.ball.country},{ball.catch_date},"  # type: ignore
+            f"{ball.trade_player.discord_id if ball.trade_player else 'None'},{ball.special},"
+            f"{ball.shiny},{ball.attack},{ball.attack_bonus},{ball.health},{ball.health_bonus}\n"
+        )
+    return BytesIO(txt.encode("utf-8"))
+
+
+async def get_trades_csv(player: PlayerModel) -> BytesIO:
+    """
+    Get a CSV file with all trades of the player.
+    """
+    trade_history = (
+        await Trade.filter(Q(player1=player) | Q(player2=player))
+        .order_by("date")
+        .prefetch_related("player1", "player2")
+    )
+    txt = "id,date,player1,player2,player1 received,player2 received\n"
+    for trade in trade_history:
+        player1_items = await TradeObject.filter(
+            trade=trade, player=trade.player1
+        ).prefetch_related("ballinstance")
+        player2_items = await TradeObject.filter(
+            trade=trade, player=trade.player2
+        ).prefetch_related("ballinstance")
+        txt += (
+            f"{trade.id},{trade.date},{trade.player1.discord_id},{trade.player2.discord_id},"
+            f"{','.join([i.ballinstance.to_string() for i in player2_items])},"  # type: ignore
+            f"{','.join([i.ballinstance.to_string() for i in player1_items])}\n"  # type: ignore
+        )
+    return BytesIO(txt.encode("utf-8"))
