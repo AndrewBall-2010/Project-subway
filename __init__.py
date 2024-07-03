c962ba1bccaebf5471f4c9f47e2e65fea1e82edb
AndrewBall-2010 <andrewm242010@gmail.com>
2024-07-03 17:22:18 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from typing import TYPE_CHECKING
 
-from ballsdex.packages.balls.cog import Balls
+from ballsdex.packages.config.cog import Config
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 
 async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(Balls(bot))
+    await bot.add_cog(Config(bot))
